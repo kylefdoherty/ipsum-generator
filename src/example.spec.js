90fb3fe28a74b2689d1e6d746199538ef41c43b7
kylefdoherty <kyledoherty22@gmail.com>
2016-04-16 11:00:10 -0400

    Setup testing with mocha and expect

---
+++ src/example.spec.js
@@ -0,0 +1,8 @@
+import expect from 'expect';
+
+describe('empty', () => {
+  it('works', () => {
+    expect(true).toEqual(true);
+  });
+});
+
---
+++ src/president.spec.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+import President from './president';
+
+describe('president', () => {
+  it('displays the displayName passed to it', () => {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<President displayName='John Doe' />)
+
+    const output = renderer.getRenderOutput();
+    const expected = <p>John Doe</p>
+
+    expect(output).toIncludeJSX(expected);
+  });
+
+  context('when selected is true', () => {
+    it('has the class z-depth-1', () => {
+
+    })
+  })
+
+  context('when selected is false', () => {
+    it('has the class z-depth-3', () => {
+
+    })
+  })
+});
